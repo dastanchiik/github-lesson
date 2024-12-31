2b6a3e96eddfcae97f3470ddfa8f0e0c8789d1df
ergasevamadina77@gmail.com <ergasevamadina77@gmail.com>
2024-12-30 21:37:23 -0800

    adtfygwrhij

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-       int a = 10;
+       /*int a = 10;
        int b = 20;
        int sum = a + b;
         System.out.println(sum);
@@ -9,8 +11,18 @@ public class Main {
         System.out.println("Kanukey Djanybekova");//kjhghjk
 
 
-        System.out.println("ulan");
+        System.out.println("ulan");*/
 
+        Random rand = new Random();
+        int[] array = new int[10];
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            array[i] = rand.nextInt(20);
+        }
+        for (int i = 0; i < 10; i++) {
+            sum +=array[i];
+        }
+        System.out.println(sum/10);
 
 
 }}
